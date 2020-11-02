483a41d34bc176b2af2ef7d7fd1f8e648d39e492
riintan <71204482+riintan@users.noreply.github.com>
2020-11-02 12:57:24 +0700

    Update msgHndlr.js

--- msgHndlr.js
+++ msgHndlr.js
@@ -83,7 +83,7 @@ module.exports = msgHandler = async (client, message) => {
         const groupAdmins = isGroupMsg ? await client.getGroupAdmins(groupId) : ''
         const isGroupAdmins = isGroupMsg ? groupAdmins.includes(sender.id) : false
         const isBotGroupAdmins = isGroupMsg ? groupAdmins.includes(botNumber + '@c.us') : false
-        const ownerNumber = '085349607186s@c.us'
+        const ownerNumber = '085349607186@c.us'
         const isOwner = ownerNumber === sender.id
         const isBlocked = blockNumber.includes(sender.id)
         const isNsfw = isGroupMsg ? nsfw_.includes(chat.id) : false
